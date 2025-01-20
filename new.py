8f848d5c4e56391d14f681dab146b55ba7e369b8
Yogesh_Dheple <yogeshdheple2022@gmail.com>
2025-01-20 05:51:02 +0530

    it is original file

---
+++ new.py
@@ -0,0 +1,20 @@
+while 'true':
+    def a():
+
+        num1 = int(input("1 st number = "))
+        num2 = int(input("2 nd number = "))
+        opr = input("oprator(+,-,*,/) = ")
+        if opr == '+':
+            print(num1 + num2)
+        elif opr == '-':
+            print(num1 - num2)
+        elif opr == '*':
+            print(num1 * num2)
+        elif opr == '/':
+            print(num1 / num2)
+        else:
+            print("invalid opr.....try again")
+
+
+    a()
+    print("thank you master")
